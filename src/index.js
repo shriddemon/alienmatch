f99249fe53d5c736b59efea41d16f8f8fd16a2e5
shriddemon <131909100+shriddemon@users.noreply.github.com>
2025-04-21 21:01:14 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import AlienMatch from "./AlienMatch";
+import "./App.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AlienMatch />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
